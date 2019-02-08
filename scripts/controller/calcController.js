8df010b55a9ff7e0741583d9fc86b1c52694873a
Douglas <dougmarques10@gmail.com>
2019-02-08 11:14:10 -0300

    add and understanding click event in app

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -6,6 +6,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -15,7 +16,16 @@ class CalcController {
             this.setDisplayDateTime();
         }, 1000);
     }
-    
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+        buttons.forEach((btn, index)=>{
+            btn.addEventListener('click', e=>{
+                console.log(index, btn.className.baseVal.replace("btn-", ""));
+            });
+        });
+    }
+
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
