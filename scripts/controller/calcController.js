d7fd3c5ff1d2ddb708ddb3f1fa980d208d251d34
Douglas <dougmarques10@gmail.com>
2019-03-08 12:07:14 -0300

    add ctrl+c and ctrl+v

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -11,6 +11,23 @@ class CalcController {
         this.initialize();
         this.initButtonsEvents();
         this.initKeyboard();
+        //this.copyToClipboard();
+    }
+
+    pastFromClipboard(){
+        document.addEventListener('paste', e => {
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc = parseFloat(text);
+        });
+    }
+
+    copyToClipboard(){
+        let input = document.createElement('input');
+        input.value = this.displayCalc;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand('Copy');
+        input.remove();
     }
 
     initialize(){
@@ -21,6 +38,7 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pastFromClipboard();
     }
 
     initKeyboard(){
@@ -59,7 +77,10 @@ class CalcController {
                 case "8":
                 case "9":
                     this.addOperation(parseInt(e.key));
-                    break;                
+                    break; 
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
+                    break;               
             }
         });
     }
